aa00e30e3c1155059fa594f88b2c9d991bf677c7
Anton Naumenko <Naumenko_anton_pnz@mail.ru>
2025-05-21 17:25:53 +0300

    Добавил тесты из первого задания.

---
+++ main.py
@@ -0,0 +1,16 @@
+from selene import browser, be, have
+
+def test_capcha_google():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('sqlex').press_enter()
+    browser.element('html').should(have.text('Об этой странице'))
+
+#browser.element('[id="search"]').should(have.text('SQL-EX'))
+
+
+def test_capcha_yandex():
+    browser.open('https://ya.ru')
+    browser.element('[name="text"]').click().type('sqlex').press_enter()
+    browser.element('html').should(have.text('Сделайте Яндекс основным поиском'))
+    #browser.all('span').by(have.exact_text('Нет, спасибо')).should(be.visible).click()
+    #browser.element('[innerHTML="sql-ex.ru"]').click()
